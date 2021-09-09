99135b3352c4f3676ee773888cc8b61b698c1cbd
julianpiniel <julianpiniel@gmail.com>
2021-09-09 19:05:32 -0300

    jwt added to all endpoints, except for login/logout

--- src/routes/Shows.js
+++ src/routes/Shows.js
@@ -2,6 +2,7 @@ const { verifyToken } = require('../middlewares/verifyToken');
 const { readShows, postShows } = require('../controllers/Shows');
 const router = require('express').Router();
 
+router.use(verifyToken);
 router.route('/').get(readShows).post(postShows);
 
 module.exports = router;
